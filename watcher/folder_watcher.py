dcbb3f09d92ab62fc43768866592a9d66d408133
Yeswanth Ram <sudarsan2507@gmail.com>
2026-03-07 23:50:29 +0530

    making the folder for already existing and going to be existed and pushing them in by organiser.py

--- watcher/folder_watcher.py
+++ watcher/folder_watcher.py
@@ -2,6 +2,8 @@ import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from classifier import get_file_category
+from organizer import move_file
 
 def is_file_stable(filepath):
 
@@ -35,15 +37,39 @@ class DownloadHandler(FileSystemEventHandler):
         try:
             if is_file_stable(file_path):
                 print(f"File is stable and ready: {file_path}")
+                category = get_file_category(file_path)
+                print(f"Category: {category}")
+                move_file(file_path, category)
             else:
                 print(f"File still downloading: {file_path}")
 
         except FileNotFoundError:
             print("File not ready yet...")
+def organize_existing_files(folder_path):
+
+    print("Scanning existing files...")
+
+    for file in os.listdir(folder_path):
+
+        file_path = os.path.join(folder_path, file)
+
+        if os.path.isdir(file_path):
+            continue
+
+        if file_path.endswith((".crdownload", ".tmp", ".part")):
+            continue
+
+        try:
+            category = get_file_category(file_path)
+            move_file(file_path, category)
+
+        except Exception as e:
+            print(f"Error processing {file_path}: {e}")
 
 if __name__ == "__main__":
 
-    path_to_watch = r"D:\Download_chrome(P1)"
+    path_to_watch = r"D:\Download_chrome(stu)"
+    organize_existing_files(path_to_watch)
 
     event_handler = DownloadHandler()
 
